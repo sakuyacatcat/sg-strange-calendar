4f0bfe4f26b1f7b002e2ffe57f7efcd1c0086973
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-17 08:08:47 +0900

    delete unnecessary variable

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -20,17 +20,16 @@ class SgStrangeCalendar
 
   def generate(vertical: false)
     header = "#{@year} #{HORIZONTAL_HEADER_SUFFIX}"
-    rows = []
-    MONTHS.each.with_index(1) do |month_name, month_index|
-      row = []
+    rows = MONTHS.each.with_index(1).map do |month_name, month_index|
       first_day = Date.new(@year, month_index, 1)
       last_day = Date.new(@year, month_index, -1)
       blanks_before_first_day = Array.new(first_day.wday, ''.rjust(HORIZONTAL_OTHER_COLUMNS_WIDTH))
 
-      row << month_name.ljust(FIRST_COLUMN_WIDTH)
-      row << blanks_before_first_day if blanks_before_first_day.any?
-      row << (first_day..last_day).each.map { |day| day.day.to_s.rjust(HORIZONTAL_OTHER_COLUMNS_WIDTH) }
-      rows << row.join(' ')
+      [
+        month_name.ljust(FIRST_COLUMN_WIDTH),
+        *blanks_before_first_day,
+        *(first_day..last_day).each.map { |day| day.day.to_s.rjust(HORIZONTAL_OTHER_COLUMNS_WIDTH) }
+      ].join(' ')
     end
     "#{header}\n#{rows.join("\n")}"
   end
