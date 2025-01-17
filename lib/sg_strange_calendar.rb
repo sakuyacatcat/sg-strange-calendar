d07f0d1ef7ca28118b10c38d418461e11d0dc341
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-17 22:10:17 +0900

    green

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -37,7 +37,7 @@ class SgStrangeCalendar
       (first_day..last_day).each do |day|
         days_info << { month: month_name, day: day }
       end
-      days_info.fill({ month: month_name, day: '' }, days_info.size..HORIZONTAL_HEADER_COLUMN.size)
+      days_info.fill({ month: month_name, day: '' }, days_info.size..(HORIZONTAL_HEADER_COLUMN.size - 1))
 
       days_info
     end
@@ -49,8 +49,10 @@ class SgStrangeCalendar
     rows = calendar_data.map do |month_data|
       month_name_str = month_data[0][:month].ljust(FIRST_COLUMN_WIDTH)
       days = ''
-      month_data.map do |day_info|
-        days += format_day(day_info[:day])
+      previous_day = ''
+      month_data.each do |day_info|
+        days += format_day(day_info[:day], previous_day)
+        previous_day = day_info[:day]
       end
 
       [month_name_str, days].join(' ').rstrip
@@ -59,22 +61,36 @@ class SgStrangeCalendar
   end
 
   def render_vertical(transposed_calendar_data)
-    # header = "#{@year} #{VERTICAL_HEADER}"
-    p transposed_calendar_data
+    header = "#{@year} #{VERTICAL_HEADER}"
+
+    rows = transposed_calendar_data.map.with_index do |date_data, index|
+      date_name_str = HORIZONTAL_HEADER_COLUMN[index].ljust(FIRST_COLUMN_WIDTH)
+      dates = ''
+      previous_day = ''
+      date_data.each do |day_info|
+        dates += format_day(day_info[:day], previous_day, vertical: true)
+        previous_day = day_info[:day]
+      end
+
+      [date_name_str, dates].join(' ').rstrip
+    end
+    "#{header}\n#{rows.join("\n")}"
   end
 
-  def format_day(day)
+  def format_day(day, previous_day, vertical: false)
+    base_width = vertical ? VERTICAL_COLUMN_WIDTH : HORIZONTAL_COLUMN_WIDTH
+
     if blank_day?(day)
-      add_width_for_blank = 1
-      return ''.rjust(HORIZONTAL_COLUMN_WIDTH + add_width_for_blank)
+      add_width_for_blank = emphasis_day?(previous_day) || blank_day?(previous_day) ? 1 : 2
+      return ''.rjust(base_width + add_width_for_blank)
     end
 
     if emphasis_day?(day)
-      add_width_for_emphasis = day.day == 1 ? 1 : 2
-      "[#{day.day}]".rjust(HORIZONTAL_COLUMN_WIDTH + add_width_for_emphasis)
+      add_width_for_emphasis = blank_day?(previous_day) ? 1 : 2
+      "[#{day.day}]".rjust(base_width + add_width_for_emphasis)
     else
-      prefix_blank = day.day == 1 || @today == day - 1 ? '' : ' '
-      "#{prefix_blank}#{day.day.to_s.rjust(HORIZONTAL_COLUMN_WIDTH)}"
+      prefix_blank = generate_prefix_blank(day, previous_day, vertical)
+      "#{prefix_blank}#{day.day.to_s.rjust(base_width)}"
     end
   end
 
@@ -84,5 +100,12 @@ class SgStrangeCalendar
 
   def emphasis_day?(day)
     day == @today
+  end
+
+  def generate_prefix_blank(_day, previous_day, _vertical)
+    return '' if blank_day?(previous_day)
+    return '' if emphasis_day?(previous_day)
+
+    ' '
   end
 end
