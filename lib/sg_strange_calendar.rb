73ccd4144e0816f3c0a330eb1af928f7bb51960b
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-17 17:03:16 +0900

    refactor

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -22,20 +22,28 @@ class SgStrangeCalendar
     rows = MONTHS.each.with_index(1).map do |month_name, month_index|
       first_day = Date.new(@year, month_index, 1)
       last_day = Date.new(@year, month_index, -1)
+
+      month = month_name.ljust(FIRST_COLUMN_WIDTH)
       blanks_before_first_day = Array.new(first_day.wday, ''.rjust(HORIZONTAL_COLUMN_WIDTH))
       days = ''
-      (first_day..last_day).each.map do |day|
-        if day == @today
-          add_width = day == first_day ? 1 : 2
-          days += "[#{day.day}]".rjust(HORIZONTAL_COLUMN_WIDTH + add_width)
+      (first_day..last_day).each do |day|
+        if emphasis_day?(day)
+          add_width_for_emphasis = day == first_day ? 1 : 2
+          days += "[#{day.day}]".rjust(HORIZONTAL_COLUMN_WIDTH + add_width_for_emphasis)
         else
           prefix_blank = day == first_day || @today == day - 1 ? '' : ' '
           days += "#{prefix_blank}#{day.day.to_s.rjust(HORIZONTAL_COLUMN_WIDTH)}"
         end
       end
 
-      [month_name.ljust(FIRST_COLUMN_WIDTH), *blanks_before_first_day, days].join(' ')
+      [month, *blanks_before_first_day, days].join(' ')
     end
     "#{header}\n#{rows.join("\n")}"
+  end
+
+  private
+
+  def emphasis_day?(day)
+    day == @today
   end
 end
